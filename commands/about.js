078845f69c5ee08f9d6f2c40a13387fd8d433b3c
Knate <email@example.com>
2018-01-20 18:43:23 -0500

    Update

--- commands/about.js
+++ commands/about.js
@@ -4,6 +4,6 @@ module.exports = {
 	args: true,
 	execute(client, message, args) {
 
-
-message.channel.send(`H-Hey there i'm ${client.user.username} i'm here to help you and make your server fun and awesome i was made by my dad(knate3#9781) and <@${config.ownerID}>`)
+const { prefix, token, ownerID } = require('../config.json');
+message.channel.send(`H-Hey there i'm ${client.user.username} i'm here to help you and make your server fun and awesome i was made by my dad(knate3#9781) and <@${ownerID}>`)
 	}}
